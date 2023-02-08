56edab40e2cc46e4b4b1f6d654d36c27028e86e8
LaV0n <leonidmedica@gmail.com>
2023-02-08 13:14:50 +0100

    feat:stylish modal Event

--- src/features/Events/EventFull/EventFull.tsx
+++ src/features/Events/EventFull/EventFull.tsx
@@ -2,6 +2,8 @@ import {Box, Modal } from '@mui/material';
 import React from 'react';
 import styles from './EventFull.module.css'
 import {EventDataType} from "../EventsReducer";
+import iconTicket from '../../../assets/icon/icons8-new-ticket-100.png'
+import {DataFormat} from "../../../common/utiles/DataFormat";
 
 type EventFullType={
     isOpen:boolean
@@ -12,6 +14,7 @@ type EventFullType={
 export const EventFull = ({isOpen,handleClose,currentEvent}:EventFullType) => {
 
     let backGroundImg=currentEvent.images.find(i=>i.height>1000)!.url
+    let isOnSale=currentEvent.dates.status.code==='onsale'
 
         return (
             <Modal open={isOpen} onClose={handleClose}>
@@ -21,30 +24,32 @@ export const EventFull = ({isOpen,handleClose,currentEvent}:EventFullType) => {
                             {currentEvent.name}
                         </div>
                         <div className={styles.place}>
-                            <p>date: {currentEvent.dates.start.localDate}</p>
-                            <p>time: {currentEvent.dates.start.localTime}</p>
-                            <p>city: {currentEvent._embedded?.venues[0].city.name}</p>
-                            <p>country: {currentEvent._embedded?.venues[0].country.name}</p>
-                            <p>{currentEvent.dates.status.code}</p>
+                            <p><span>date:</span> {DataFormat(currentEvent.dates.start.localDate)}</p>
+                            <p><span>time:</span> {currentEvent.dates.start.localTime}</p>
+                            <p><span>city:</span> {currentEvent._embedded?.venues[0].city.name} ({currentEvent._embedded?.venues[0].country.name})</p>
+                            <p><span>place:</span> {currentEvent._embedded.venues[0].name}</p>
                         </div>
                     </div>
-                    {
-                        currentEvent.seatmap &&
-                        <img src={currentEvent.seatmap.staticUrl} alt="0" className={styles.seatmapImg}/>
-                    }
-
-                    <div>
-                        { currentEvent.priceRanges?.map(p=>
-                                <div key={p.min}>
-                                    {p.type}
-                                    {p.min}
-                                    {p.max}
-                                    {p.currency}
+                    <div className={styles.priceBlock}>
+                        <div className={styles.status}
+                             style={{border:`3px solid ${isOnSale?'green':'red'}`}}>
+                            {currentEvent.dates.status.code}
+                        </div>
+                        { currentEvent.priceRanges?.map((p,index)=>
+                                <div className={styles.prices} key={index}>
+                                    <div>{p.type}</div>
+                                    <div>{p.min} - {p.max} {p.currency}</div>
                                 </div>
                             )
                         }
+                        <a href={currentEvent.url}>
+                            <img src={iconTicket} alt="0" className={styles.ticketIcon}/>
+                        </a>
+                        {
+                            currentEvent.seatmap &&
+                            <img src={currentEvent.seatmap.staticUrl} alt="0" className={styles.seatmapImg}/>
+                        }
                     </div>
-                    <a href={currentEvent.url}>link</a>
                 </Box>
             </Modal>
         );
