7983eb470f5d27a034d1bedfbcb8ccd3c89748e0
LaV0n <leonidmedica@gmail.com>
2023-02-13 10:44:06 +0100

    feat: add header style

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import logoImg from './../../assets/img/10368_2.jpg'
 
 export const Header = () => {
     return (
         <div className={styles.container}>
-            header
-            <NavLink to={'/joke'} >   joke   </NavLink>
-            <NavLink to={'/'} >   Events   </NavLink>
+            <img src={logoImg} alt="0" className={styles.logoImg}/>
+            <NavLink to={'/joke'} className={styles.link}>Jokes</NavLink>
+            <NavLink to={'/'}  className={styles.link}>Events</NavLink>
         </div>
     );
 };
